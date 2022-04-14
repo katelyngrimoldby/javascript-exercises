a72de631a223b4f205f353e6681f76a4061bfebd
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-04-13 21:36:52 -0600

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degree) {
+  degree = (degree - 32) * (5/9);
+  return degree = Math.round(degree*10)/10
 };
 
-const ctof = function() {
-
+const ctof = function(degree) {
+  degree = (degree * (9/5)) + 32;
+  return degree = Math.round(degree*10)/10
 };
 
 // Do not edit below this line
