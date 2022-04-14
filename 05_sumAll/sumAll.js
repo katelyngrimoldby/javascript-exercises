cfe409e296bdc5f7e6d41ba6c7a08f2423db2d68
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-04-13 21:16:58 -0600

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let num = 0;
+
+    if(typeof num1 === typeof num2){
+        if(num1 > 0 && num2 > 0){
+            if(num1 > num2){
+                for(let i=num1; i>=num2; i--){
+                    num = num + i
+                }
+                return num
+            } else {
+                for(let i=num1; i<=num2; i++){
+                    num = num + i
+                }
+                return num
+            }
+        }
+    }
+    return 'ERROR'
 };
 
 // Do not edit below this line
