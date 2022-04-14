b1cb861b9f27a67cb1c2ba42133db5038015b715
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-04-13 21:02:22 -0600

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    arr = args[0]
+    let newArray = []
+
+    arr.forEach((element) => {
+        if(!args.includes(element)){
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
