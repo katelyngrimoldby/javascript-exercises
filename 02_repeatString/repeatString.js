610924a102d9bc6937b3aaa23d8b47aac8c5360a
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-04-13 19:49:08 -0600

    done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = ''
 
+    if(num < 0){
+        return newString = 'ERROR';
+    }
+    for(i=0; i < num; i++){
+        newString = newString.concat(string);
+    }
+    return newString;
 };
-
+console.log(repeatString)
 // Do not edit below this line
 module.exports = repeatString;
